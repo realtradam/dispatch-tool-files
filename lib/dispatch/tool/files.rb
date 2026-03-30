25488d32336e05b69a41391cc7b5153478d3cc8a
Adam Malczewski <github@tradam.dev>
2026-03-31 00:52:33 +0900

    init

---
+++ lib/dispatch/tool/files.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative "files/version"
+
+module Dispatch
+  module Tool
+    module Files
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ spec/dispatch/tool/files_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Dispatch::Tool::Files do
+  it "has a version number" do
+    expect(Dispatch::Tool::Files::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
